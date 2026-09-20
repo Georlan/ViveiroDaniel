8f7131418b89552285478d10e1fc529350548f56
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-20 20:47:42 -0300

    feat: add pond domain types

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export type BiometryInput = {
+  id: string
+  date: string
+  currentWeightG: number
+  feedRatePercent: number
+  dailyFeedKg: number
+  accumulatedFeedKg: number
+}
+
+export type Pond = {
+  id: string
+  name: string
+  areaHa: number
+  initialPopulation: number
+  laboratory: string
+  stockingDate: string
+  cycleStartDate: string
+  cycle: number
+  feeder: string
+  plPerGram: number | null
+  biometries: BiometryInput[]
+}
+
+export type CalculatedBiometry = BiometryInput & {
+  cultivationDay: number
+  previousWeightG: number | null
+  growthG: number
+  averageGrowthPerWeekG: number
+  feedFor100Kg: number
+  survivalPercent: number
+  biomassKg: number
+  fca: number
+}
